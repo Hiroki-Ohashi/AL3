aed9da10a23d542f4d783869b94076502ec433ea
洋輝 大橋 <hiro0084169@outlook.jp>
2023-05-10 17:52:13 +0900

    hozon

---
+++ Player.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+#include <cassert>
+
+
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	assert(model);
+	model_ = model;
+	textureHandle_ = textureHandle;
+	// ワールドトランスフォームの初期化
+	worldTransform_->Initialize();
+}
+
+void Player::Update() { 
+	worldTransform_->TransferMatrix();
+}
+
+void Player::Draw(ViewProjection* viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
